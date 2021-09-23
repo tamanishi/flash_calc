b227b4a1f7fed1e504c0b402d3ab4e0d87fc0b51
Masayuki Sunahara <tamanishi@gmail.com>
2021-09-23 20:12:41 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
 
+const interval = 1300;
+
 function App() {
+  const [count, setCount] = useState(0);
+  const [answer, setAnswer] = useState([0, 0, 0]);
+
+  useEffect(() => {
+    let id = setInterval(() => {
+      setCount(count + 1);
+    }, interval);
+
+    if (count % 2 === 0) {
+      let x = Math.floor(Math.random() * 10);
+      let y = Math.floor(Math.random() * 10);
+      setAnswer([x, y]);
+    } else {
+      setAnswer([answer[0], answer[1], answer[0] + answer[1]]);
+    }
+
+    return () => {
+      clearInterval(id);
+    };
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <p>{answer[0]} + {answer[1]} = {answer[2]}</p>
     </div>
   );
 }
